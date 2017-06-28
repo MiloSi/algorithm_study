848e8e7a02a8dd5c593b2ed7b4b86d7a7ffed767
C <aquarius456@nate.com>
2017-06-28 09:06:19 +0900

    Update main.cpp

--- baekjoon_1003/main.cpp
+++ baekjoon_1003/main.cpp
@@ -1,70 +1,25 @@
-/*
-You should use the statndard input/output
-
-in order to receive a score properly.
-
-Do not use file input and output
-
-Please be very careful.
-*/
-
-#pragma warning (disable : 4996)
+//#pragma warning (disable : 4996)
 
 #include <iostream>
-#include <memory.h>
-
-using namespace std;
-
-int Answer;
-
 int main(int argc, char** argv)
 {
-	int T, test_case;
-	int arr[101][101];
-
-	int tmax = 0;
+	int t = 0;
 	int n = 0;
-	int sum;
-	int v;
-
-	cin >> T;
-	for (test_case = 0; test_case < T; test_case++)
-	{
-		cin >> n; 
-		sum = 0;
-
-		memset(arr, 0, 101 * 101 * sizeof(int));
-		for (int j = 1; j <= n; j++)
-		{
-			for (int i = 1; i <= n; i++)
-			{
-				cin >> v;
-				arr[j][i] = v + arr[j][i-1];
-			}
-		}
-		Answer = -500000;
-		for (int x = 1; x <= n; x++)
-		{
-
-			for (int i = x; i <= n; i++)
-			{
-				sum = 0;
-				for (int j = x; j <=  n; j++)
-				{
-					v = arr[j][i] - arr[j][x-1];
-
-					sum = v + sum > sum ? v + sum : sum;
-					Answer = sum > Answer ? sum : Answer;
-				}
-			}
-		}
-
-
-
+	int zero[41] = {1, 0,};
+	int ones[41] = {0, 1,};
 	
-		cout << "Case #" << test_case + 1 << endl;
-		cout << Answer << endl;
+	
+	for(int i = 2; i <= 40; i++)
+	{
+		zero[i] = zero[i-1] + zero[i-2];
+		ones[i] = ones[i-1] + ones[i-2];
 	}
-
-	return 0;//Your program should return 0 on normal termination.
+	cin >> t;
+	for(int i = 0; i < t; i++ )
+	{
+		cin >> n;
+		cout << zero[n] << " " << ones[n] << endl;
+	}
+	
+	return 0;
 }
