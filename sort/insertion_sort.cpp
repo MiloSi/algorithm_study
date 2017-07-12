ac3cfd0ddbf51b72478c8ce7d14126e8ccf59992
C <aquarius456@nate.com>
2017-07-12 15:32:31 +0900

    Add files via upload

---
+++ sort/insertion_sort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+#define SIZE 12
+#define N SIZE
+
+inline void SWAP(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+void insertion_sort(int  *arr)
+{
+	int temp;
+	int j;
+	for (int i = 1; i < N; i++)
+	{
+		temp = arr[i];
+		for (j = i - 1; j >= 0; j--)
+		{
+			if (arr[j] > temp) arr[j + 1] = arr[j];
+			else break;
+		}
+		arr[j + 1] = temp;
+	}
+
+
+}
+
+void show(int *arr)
+{
+	for (int i = 0; i < N; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+	int arr[SIZE] = { 1, 11, 2, 8, 4, 19, 3, 42, 5, 7, 10, 6 };
+
+	cout << "INSERTON SORT" << endl;
+	show(arr);
+	insertion_sort(arr);
+	show(arr);
+
+	return 0;
+}
+
