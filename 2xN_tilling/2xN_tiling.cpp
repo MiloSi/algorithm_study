e755d36157d55cbfd0b7d36c64525c40b25a9013
C <aquarius456@nate.com>
2017-07-19 17:05:47 +0900

    Add files via upload

---
+++ 2xN_tilling/2xN_tiling.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int r;
+
+	cin >> n;
+
+	if (n == 1) cout << 1 << endl;
+	else if (n == 2) cout << 2 << endl;
+	else {
+
+		int a = 1;
+		int b = 2;
+		for (int i = 3; i <= n; i++)
+		{
+			r = (a + b) % 10007;
+			a = b % 10007;
+			b = r;
+		}
+		cout << r << endl;
+	}
+
+
+
+	return 0;
+}
+
