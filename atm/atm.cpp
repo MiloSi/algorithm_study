768b5aea130a79c0e03b78fc83aa6d322ae6dc9e
C <aquarius456@nate.com>
2017-07-25 00:20:23 +0900

    Add files via upload

---
+++ atm/atm.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int table[1000];
+
+
+int main() {
+	int n;
+	int sum = 0;
+	int total = 0;
+
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> table[i];
+
+	sort(table, table + n);
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += table[i];
+		total += sum;
+	}
+
+
+	cout << total << endl;
+
+	return 0;
+}
