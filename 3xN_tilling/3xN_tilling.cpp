17edc0fd0cdaa6c33f6712925b38abab96563d31
C <aquarius456@nate.com>
2017-07-31 17:13:27 +0900

    Update 3xN_tilling.cpp

--- 3xN_tilling/3xN_tilling.cpp
+++ 3xN_tilling/3xN_tilling.cpp
@@ -1,23 +1,22 @@
-//#include <iostream>
-//
-//long long table[31];
-//int main() {
-//	int n;	
-//	table[0] = 1;
-//	table[1] = 0;
-//	table[2] = 3;
-//
-//	std::cin >> n;
-//
-//	for (int i = 4; i <= n; i += 2)
-//	{
-//		table[i] = table[i - 2] * 3;
-//		for (int j = 4; j <= i; j += 2) {
-//			table[i] += table[i - j] * 2;
-//		}
-//	}
-//
-//	std::cout << table[n] << std::endl;
-//
-//	return 0;
-//}
+#include <iostream>
+long long table[31];
+int main() {
+	int n;	
+	table[0] = 1;
+	table[1] = 0;
+	table[2] = 3;
+
+	std::cin >> n;
+
+	for (int i = 4; i <= n; i += 2)
+	{
+	  table[i] = table[i - 2] * 3;
+		for (int j = 4; j <= i; j += 2) {
+			table[i] += table[i - j] * 2;
+		}
+	}
+
+	std::cout << table[n] << std::endl;
+
+	return 0;
+}
